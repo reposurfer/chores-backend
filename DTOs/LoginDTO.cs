d8a2e0c1ed30ca35fab0f6473e2e23adc9fb5210
Stef Boerjan <stef.boerjan@gmail.com>
2023-07-18 16:18:26 +0200

    error handling

--- DTOs/LoginDTO.cs
+++ DTOs/LoginDTO.cs
@@ -4,22 +4,26 @@ namespace chores_backend.DTOs;
 
 public class LoginDTO
 {
-    [Required]
+    [Required(ErrorMessage = "The username field is required.")]
     [StringLength(200)]
     public string Username { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = "The password field is required.")]
     [RegularExpression("^(?=.*[A-Za-z])(?=.*\\d).{5,}$", ErrorMessage = "Password must be at least 5 characters and contain at least one letter and number.")]
     public string Password { get; set; }
 }
 
 public class RegisterDTO : LoginDTO
 {
-    [Required]
+    [Required(ErrorMessage = "The firstname field is required.")]
     public string FirstName { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = "The lastname field is required.")]
     public string LastName { get; set; }
+    
+    [Required(ErrorMessage = "The confirm password field is required.")]
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+    public string ConfirmPassword { get; set; }
     
     [Required]
     public ICollection<string> Roles { get; set; }
